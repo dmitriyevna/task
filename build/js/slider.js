967b2538a5b886f359fbc9f293dd61106b7e9529
Penkova Yana <dmitriyevna.y@gmail.com>
2017-04-10 03:12:10 +0300

    Test task
    adding project files

---
+++ build/js/slider.js
@@ -0,0 +1,25 @@
+
+var slideIndex = 1;
+
+showDivs(slideIndex);
+
+function plusDivs(n) {
+
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+
+    var x = document.getElementsByClassName("headerSlider__slide");
+
+    if ( n > x.length ) { slideIndex = 1; } 
+
+    if (n < 1) { slideIndex = x.length; }
+
+    for ( var i = 0; i < x.length; i++ ) {
+
+        x[i].style.display = "none"; 
+    }
+    
+    x[slideIndex-1].style.display = "block"; 
+}
